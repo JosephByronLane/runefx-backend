6e44c9e797ff1b09c84e160e869f876e62d886f6
JosephByronLane <24197141@modelo.edu.mx>
2025-05-17 17:52:29 -0600

    base subtopics serializer

--- forum/serializers.py
+++ forum/serializers.py
@@ -13,3 +13,25 @@ class TopicSerializer(serializers.ModelSerializer):
         model = Topic
         fields = ['id', 'title', 'description','subtopics']
 
+
+
+class SubtopicSerializer(serializers.ModelSerializer):
+    posts = serializers.SerializerMethodField()
+    child_subtopics = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Subtopic
+        fields = ['id', 'title', 'description', 'parent_subtopic','child_subtopics']
+
+    def get_child_subtopics(self,obj):
+        children = Subtopic.objects.filter(parent_subtopic=obj.id)
+        return SubtopicSerializer(children, many=True, context=self.context).data
+    
+    def validate(self, data):
+        if 'parent_topic' not in data and 'parent_subtopic' not in data:
+            raise serializers.ValidationError("Subtopic must belong to a Topic, or Subtopic.")
+        
+        if 'parent_subtopic' in data and 'parent_topic' in data:
+            raise serializers.ValidationError("Subtopic cannot belong to both a Topic and Subtopic.")
+        
+        return data
