95b8e4c090ee6943466fbd68952df1e8f6fb300c
JosephByronLane <24197141@modelo.edu.mx>
2025-05-17 18:14:17 -0600

    base forum views

--- forum/views.py
+++ forum/views.py
@@ -1,2 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Topic, Subtopic, Post, Comment
+from .serializers import TopicSerializer, SubtopicSerializer, PostSerializer, CommentSerializer
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class SubtopicViewSet(viewsets.ModelViewSet):
+    queryset = Subtopic.objects.all()
+    serializer_class = SubtopicSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
