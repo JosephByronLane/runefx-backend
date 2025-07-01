8929d824fb7892698c2621222cdbc58c21ecfdf6
JosephByronLane <24197141@modelo.edu.mx>
2025-06-30 22:42:09 -0600

    release models

--- releases/models.py
+++ releases/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+class Release(models.Model):
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False
+    )
 
-# Create your models here.
+    created_at = models.DateTimeField(
+        auto_now_add=True
+
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING,
+        blank=False,
+        null=False
+    )
+
+    content = models.TextField(
+        blank=False,
+        null=False,
+        help_text="Release note's content. Markdown supported"
+    )
+
+
