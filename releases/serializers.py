6ae35c38941ed98cbfd9df6dc6f143cafcf4d7f5
JosephByronLane <24197141@modelo.edu.mx>
2025-06-30 23:34:52 -0600

    made endpoints return user data

--- releases/serializers.py
+++ releases/serializers.py
@@ -2,23 +2,34 @@ from rest_framework import serializers
 from .models import Release
 
 class ReleaseSerializer(serializers.ModelSerializer):
-    
-
+    created_by_user_data = serializers.SerializerMethodField()
     class Meta:
         model = Release
-        fields = ['id', 'title','created_at', 'created_by', 'content', 'showcase_picture_url']
+        fields = ['id', 'title','created_at', 'created_by', 'content', 'showcase_picture_url', 'created_by_user_data']
         read_only_fields = ['created_at', 'created_by']
+
+    def get_created_by_user_data(self, obj):        
+        if obj.created_by:
+            return {
+                'username': obj.created_by.username,
+                'user_pfp_url': obj.created_by.profile_picture_url
+            }
 
 
 class ReleaseSerializerWithoutContent(serializers.ModelSerializer):
-
-
+    created_by_user_data = serializers.SerializerMethodField()
 
     class Meta:
         model = Release
-        fields = ['id', 'title', 'created_at', 'created_by', 'showcase_picture_url']
+        fields = ['id', 'title', 'created_at', 'created_by', 'showcase_picture_url', 'created_by_user_data']
         read_only_fields = ['created_at', 'created_by']
 
 
+    def get_created_by_user_data(self, obj):        
+        if obj.created_by:
+            return {
+                'username': obj.created_by.username,
+                'user_pfp_url': obj.created_by.profile_picture_url
+            }
 
     
