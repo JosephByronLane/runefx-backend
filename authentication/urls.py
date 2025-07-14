6dacb761b6a9a4a325b52412cc86c5395ec52e5e
JosephByronLane <24197141@modelo.edu.mx>
2025-07-14 17:09:29 -0600

    made the ci a single job

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    #path('register/', RegisterView.as_view(), name='register'),
     path('login/', CookieTokenObtainPairView.as_view(), name='login'),
     path('login/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('verify-token/', TokenVerifyView.as_view(), name='verify-token'),
